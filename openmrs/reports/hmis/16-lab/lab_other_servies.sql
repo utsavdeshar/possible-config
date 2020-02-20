fbc38e3cec3e6eacb4724077f9463128f6f469b7
suruchee <suruchee11@gmail.com>
2020-02-20 16:35:20 +0545

    lab query changed acc to new requirement

--- openmrs/reports/hmis/16-lab/lab_other_servies.sql
+++ openmrs/reports/hmis/16-lab/lab_other_servies.sql
@@ -1,15 +1,28 @@
 SELECT 
 SUM(c1)as protein_count,
-SUM(final.c2) as other_count,
+SUM(final.c2) as other_bio_chemistry_count,
 SUM(final.c3) as gm_stain_count,
 SUM(final.c4) as body_fluid_culture_count,
 SUM(final.c5) as body_fluid_afb_count,
-SUM(final.c6) as TPHA,
+SUM(final.c6) as TPHA_total,
+SUM(final.c6p) as TPHA_Positive,
+SUM(final.c6n) as TPHA_Negative,
 SUM(final.c7) as Sugar_F,
 SUM(final.c8) as Sugar_PP,
 SUM(final.c9) as Sugar_R,
-SUM(final.c10) as HIV,
-SUM(final.c11) as PT_INR
+SUM(final.c10) as HIV_Total,
+SUM(final.c10p) as HIV_Positive,
+SUM(final.c10n) as HIV_Negative,
+SUM(final.c11) as PT_INR,
+SUM(final.c12) as Other_Hematology,
+SUM(final.c13) as HBsAg_Total,
+SUM(final.c13p) as HBsAg_Positive,
+SUM(final.c13n) as HBsAg_Negative,
+SUM(final.c14) as HCV_total,
+SUM(final.c14p) as HCV_Postive,
+SUM(final.c14n) as HCV_Negative,
+SUM(final.c15) as CPK_NAC,
+SUM(final.c16) as AFBsputum
 FROM
 -- --------------------------- all protein--------------------------
 (SELECT
@@ -19,11 +32,24 @@ SUM(total_count1) as c1,
 0 as c4,
 0 as c5,
 0 as c6,
+0 as c6p,
+0 as c6n,
 0 as c7,
 0 as c8,
 0 as c9,
 0 as c10,
-0 as c11
+0 as c10p,
+0 as c10n,
+0 as c11,
+0 as c12,
+0 as c13,
+0 as c13p,
+0 as c13n,
+0 as c14,
+0 as c14p,
+0 as c14n,
+0 as c15,
+0 as c16,
 FROM
 (SELECT DISTINCT
 
@@ -42,10 +68,10 @@ FROM clinlims.test_section ts
  
 GROUP BY ts.name, t.name, t.id
 order by ts.name) as protein
--- --------------------------other---------------------
+-- --------------------------other bio-chemistry---------------------
 UNION ALL
 SELECT
-0,SUM(total_count2) as c2,0,0,0,0,0,0,0,0,0
+0,SUM(total_count2) as c2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -60,14 +86,15 @@ FROM clinlims.test_section ts
   LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
   LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
   LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
-  WHERE t.name IN ('ADA (Pleural_Fluid)','ADA (Peritoneal_Fluid)','ADA (Pericardial_Fluid)','ADA (Serum)','ADA (CSF)')
+  WHERE t.name IN ('ADA (Pleural_Fluid)','ADA (Peritoneal_Fluid)','ADA (Pericardial_Fluid)','ADA (Serum)','ADA (CSF)','ADA (Synovial Fluid)',
+		'75g OGTT 2 hours','75g OGTT FASTING','75g OGTT','75g OGTT 1 hour','Chloride')
  
 GROUP BY ts.name, t.name, t.id
-order by ts.name) as other
+order by ts.name) as other_bio_chemistry_count
 -- ---------------------gm_stain-------------------------
 UNION ALL
 SELECT
-0,0,SUM(total_count3) as c3,0,0,0,0,0,0,0,0
+0,0,SUM(total_count3) as c3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -90,7 +117,7 @@ order by ts.name) as gm_stain
 -- --------------body_fluid_culture------------------------------
 UNION ALL
 SELECT
-0,0,0,SUM(total_count4) as c4,0,0,0,0,0,0,0
+0,0,0,SUM(total_count4) as c4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -112,7 +139,7 @@ order by ts.name) as body_fluid_culture
 -- ----------------body_fluid_afb--------------------
 UNION ALL
 SELECT
-0,0,0,0,SUM(total_count5) as c5,0,0,0,0,0,0
+0,0,0,0,SUM(total_count5) as c5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -135,7 +162,7 @@ order by ts.name) as body_fluid_afb
 ----------TPHA------------
 UNION ALL
 SELECT
-0,0,0,0,0,SUM(total_count6) as c6,0,0,0,0,0
+0,0,0,0,0,SUM(total_count6) as c6,SUM(positive) as c6p, SUM(negative) as c6n,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -157,7 +184,7 @@ order by ts.name) as TPHA
 -------------Sugar F---------
 UNION ALL
 SELECT
-0,0,0,0,0,0,SUM(total_count7) as c7,0,0,0,0
+0,0,0,0,0,0,0,0,SUM(total_count7) as c7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -179,7 +206,7 @@ order by ts.name) as Sugar_F
 -------------Sugar PP---------
 UNION ALL
 SELECT
-0,0,0,0,0,0,0,SUM(total_count8) as c8,0,0,0
+0,0,0,0,0,0,0,0,0,SUM(total_count8) as c8,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -201,7 +228,7 @@ order by ts.name) as Sugar_PP
 -------------Sugar R---------
 UNION ALL
 SELECT
-0,0,0,0,0,0,0,0,SUM(total_count9) as c9,0,0
+0,0,0,0,0,0,0,0,0,0,SUM(total_count9) as c9,0,0,0,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -223,7 +250,7 @@ order by ts.name) as Sugar_R
 --------------HIV-----------
 UNION ALL
 SELECT
-0,0,0,0,0,0,0,0,0,SUM(total_count10) as c10,0
+0,0,0,0,0,0,0,0,0,0,0,SUM(total_count10) as c10,SUM(positive) as c10p, SUM(negative) as c10n,0,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -245,7 +272,7 @@ order by ts.name) as HIV
 -----------------PT-INR---------
 UNION ALL
 SELECT
-0,0,0,0,0,0,0,0,0,0,SUM(total_count11) as c11
+0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count11) as c11,0,0,0,0,0,0,0,0,0
 FROM
 (SELECT DISTINCT
 
@@ -264,4 +291,117 @@ FROM clinlims.test_section ts
 
 GROUP BY ts.name, t.name, t.id
 order by ts.name) as PT_INR
+-----------------Other Haematology---------
+UNION ALL
+SELECT
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count12) as c12,0,0,0,0,0,0,0,0
+FROM
+(SELECT DISTINCT
+
+  ts.name       AS department,
+  t.name        AS test12,
+  count(r.id)   AS total_count12,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r1.id) ELSE NULL END AS positive,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r2.id) ELSE NULL END AS negative
+FROM clinlims.test_section ts
+  INNER JOIN clinlims.test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
+  LEFT OUTER JOIN clinlims.analysis a ON t.id = a.test_id
+  LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
+  LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
+  LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
+ WHERE t.name IN ('Cross Match-major','Absolute Eosinophil Count')
+GROUP BY ts.name, t.name, t.id
+order by ts.name) as Other_Hematology
+-----------HBsAg--------------
+UNION ALL
+SELECT
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count13) as c13,SUM(positive) as c13p, SUM(negative) as c13n,0,0,0,0,0
+FROM
+(SELECT DISTINCT
+
+  ts.name       AS department,
+  t.name        AS test11,
+  count(r.id)   AS total_count11,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r1.id) ELSE NULL END AS positive,
+
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r2.id) ELSE NULL END AS negative
+FROM clinlims.test_section ts
+  INNER JOIN clinlims.test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
+  LEFT OUTER JOIN clinlims.analysis a ON t.id = a.test_id
+  LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
+  LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
+  LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
+  WHERE t.name IN ('HBsAg (Serum)','HBsAg (Blood)')
+
+GROUP BY ts.name, t.name, t.id
+order by ts.name) as HBsAg
+-----------------HCV---------
+UNION ALL
+SELECT
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count14) as c14,SUM(positive) as c14p, SUM(negative) as c14n,0,0
+FROM
+(SELECT DISTINCT
+
+  ts.name       AS department,
+  t.name        AS test11,
+  count(r.id)   AS total_count11,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r1.id) ELSE NULL END AS positive,
+
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r2.id) ELSE NULL END AS negative
+FROM clinlims.test_section ts
+  INNER JOIN clinlims.test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
+  LEFT OUTER JOIN clinlims.analysis a ON t.id = a.test_id
+  LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
+  LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
+  LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
+  WHERE t.name IN ('HCV (Blood)','HCV (Serum)')
+
+GROUP BY ts.name, t.name, t.id
+order by ts.name) as HCV
+-----------------CPK-NAC---------
+UNION ALL
+SELECT
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count15) as c15,0
+FROM
+(SELECT DISTINCT
+
+  ts.name       AS department,
+  t.name        AS test11,
+  count(r.id)   AS total_count11,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r1.id) ELSE NULL END AS positive,
+
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r2.id) ELSE NULL END AS negative
+FROM clinlims.test_section ts
+  INNER JOIN clinlims.test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
+  LEFT OUTER JOIN clinlims.analysis a ON t.id = a.test_id
+  LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
+  LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
+  LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
+  WHERE t.name IN ('CK-NAC','CPK')
+
+GROUP BY ts.name, t.name, t.id
+order by ts.name) as CPK_NAC
+-----------------AFB---------
+UNION ALL
+SELECT
+0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,SUM(total_count16) as c16
+FROM
+(SELECT DISTINCT
+
+  ts.name       AS department,
+  t.name        AS test11,
+  count(r.id)   AS total_count11,
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r1.id) ELSE NULL END AS positive,
+
+  CASE WHEN t.id IN (SELECT test_id FROM clinlims.test_result WHERE tst_rslt_type = 'D') THEN count(r2.id) ELSE NULL END AS negative
+FROM clinlims.test_section ts
+  INNER JOIN clinlims.test t ON ts.id = t.test_section_id AND t.is_active = 'Y'
+  LEFT OUTER JOIN clinlims.analysis a ON t.id = a.test_id
+  LEFT OUTER JOIN clinlims.result r ON a.id = r.analysis_id and cast(r.lastupdated as date) BETWEEN '#startDate#' AND '#endDate#' and r.value != ''
+  LEFT OUTER JOIN clinlims.result r1 ON r1.result_type = 'D' and r1.value != '' and r.id=r1.id and r1.abnormal=true
+  LEFT OUTER JOIN clinlims.result r2 on r2.result_type = 'D' and r2.value != '' and r.id=r2.id and r2.abnormal=false
+  WHERE t.name IN ('AFB Sputum A','AFB Sputum B')
+
+GROUP BY ts.name, t.name, t.id
+order by ts.name) as AFBsputum
 ) final
